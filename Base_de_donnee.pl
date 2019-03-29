2b449998934a27d9a5970f630c3888d9c1e45334
Domitille Coq Etchegaray <dcetchegaray@emi.u-bordeaux1.fr>
2019-03-29 18:01:55 +0100

    Bdd_Insertion

--- Base_de_donnee.pl
+++ Base_de_donnee.pl
@@ -5,12 +5,12 @@ use DBI;
 
 #Connexion de la base de donnee
 my $dbh=DBI->connect("DBI:Pg:dbname=dcetchegaray;host=dbserver","dcetchegaray","b2wehd7t",{'RaiseError' => 1});
-$dbh->do("drop table GeneNameUniprot cascade");
-$dbh->do("drop table ProteinNameUniprot cascade");
+$dbh->do("drop table NameUniprot cascade");
 $dbh->do("drop table EntreeUniprot cascade");
 $dbh->do("drop table FichierEnsembl cascade");
 #Creation des tables et de leurs attributs
-$dbh->do("create table GeneNameUniprot(EntryName text constraint cle_Gene primary key, GeneName text, Synonymous text, GeneOntology text)");
-$dbh->do("create table ProteinNameUniprot(EntryName text constraint cle_Prot primary key references GeneNameUniprot(EntryName), ProteineName text, GeneName text, Sequence text, Statut varchar(25) constraint R_U check(Statut in ('reviewed','unreviewed')), Length int)");
-$dbh->do("create table EntreeUniprot(Entry varchar(100) constraint cle_Entree primary key, EntryName text references ProteinNameUniprot(EntryName), Organism text, EnsemblPlants text)");
-$dbh->do("create table FichierEnsembl(PlantReactomeReactionID text, UniprotKBTrEMBLID varchar(100), TranscriptStableID varchar(100), GeneStableID varchar(100), constraint cle_Uni primary key(PlantReactomeReactionID, TranscriptStableID))");
+$dbh->do("create table NameUniprot(EntryName text constraint cle_EntreeNom primary key, GeneName text, Synonymous text, GeneOntology text, ProteineName text, Sequence text, Statut varchar(25) constraint R_U check(Statut in ('reviewed','unreviewed')), Length int, ECNumber text)");
+$dbh->do("create table EntreeUniprot(Entry varchar(100) constraint cle_Entree primary key, EntryName text references NameUniprot(EntryName), Organism text, EnsemblPlants text)");
+$dbh->do("create table FichierEnsembl(UniprotKBTrEMBLID varchar(100) constraint cle_Fichiermart primary key, TranscriptStableID varchar(100), GeneStableID varchar(100), PlantReactomeId text)");
+
+$dbh->disconnect();
--- Insertion_BDD.pl
+++ Insertion_BDD.pl
@@ -3,58 +3,66 @@ use strict;
 use warnings;
 use DBI;
 
+
 #Connexion de la base de donnee
 my $dbh=DBI->connect("DBI:Pg:dbname=dcetchegaray;host=dbserver","dcetchegaray","b2wehd7t",{'RaiseError' => 1});
 
-open(IN,"../uniprot-arabidopsisthalianaSequence.tab")|| die "No file";
+open(IN,"../uniprot-arabidopsisthalianaSequence.tab") || die "No file";
 my $l1=0;
-my $NaNgene=0;
-my $NaNprot=0;
 while(<IN>){
     $l1++;
     $_=~s/'//g;
     if($l1!=1){
 	my @val=split(/\t/,$_);
-	my $entry="'$val[0]'";
-	my $entry_name="'$val[1]'";
-	my $statut="'$val[2]'";
-	my $protein_name="'$val[3]'";
-	my $gene_name="'$val[4]'";
 	my $organism="'$val[5]'";
-	my $length=int($val[6]);
-	my $gene_namesyn="'$val[7]'";
-	my $gene_ontology="'$val[8]'";
-	my $ensembl="'$val[9]'";
-	my $sequence="'$val[10]'";
-	$dbh->do("insert into GeneNameUniprot values($entry_name, $gene_name, $gene_namesyn, $gene_ontology)");
-	$dbh->do("insert into ProteinNameUniprot values($entry_name, $protein_name, $gene_name, $sequence, $statut, $length)");
-	$dbh->do("insert into EntreeUniprot values($entry, $entry_name, $organism, $ensembl)");
+	if($organism=~/Arabidopsis thaliana/){
+	    print "$organism \n";
+	    my $ec="'NaN'";
+	    my $entry="'$val[0]'";
+	    my $entry_name="'$val[1]'";
+	    my $statut="'$val[2]'";
+	    my $protein_name="'$val[3]'";
+	    if($protein_name=~/(EC\s(\d+|\-)\.(\d+|\-)\.(\d+|\-)\.(\d+|\-))/){
+		$ec="'$1'";
+	    }
+	    my $gene_name="'$val[4]'";
+	    my $length=int($val[6]);
+	    my $gene_namesyn="'$val[7]'";
+	    my $gene_ontology="'$val[8]'";
+	    my $ensembl="'$val[9]'";
+	    my $sequence="'$val[10]'";
+	    $dbh->do("insert into NameUniprot values($entry_name, $gene_name, $gene_namesyn, $gene_ontology, $protein_name, $sequence, $statut, $length, $ec)");
+	    $dbh->do("insert into EntreeUniprot values($entry, $entry_name, $organism, $ensembl)");
+	}
     }
 }
 close(IN);
 
-open(IN_2,"../mart_export.csv")|| die "No file";
+
+open(IN_2,"../mart_export.csv") || die "No file";
 my $l1_2=0;
-my $Nan=0;
+my @doublon;
+push(@doublon,"");
 while(<IN_2>){
     $l1_2++;
     $_=~s/'//g;
     if($l1_2!=1){
 	my @val=split(/,/,$_);
-	my $GeneStable="'$val[0]'";
-	my $Transcript="'$val[1]'";
 	my $Uniprot="'$val[2]'";
-	if($Uniprot=~/''/){
-	    $Nan++;
-	    $Uniprot="'NaN$Nan'";
+	my $test_doublon=($Uniprot ~~ @doublon);
+	if($test_doublon eq ""  && $Uniprot ne ''){
+	    my $GeneStable="'$val[0]'";
+	    my $Transcript="'$val[1]'";
+	    my $PlantReac="'$val[3]'";
+	    $dbh->do("insert into FichierEnsembl values($Uniprot, $Transcript, $GeneStable, $PlantReac)");
 	}
-	my $PlantReac="'$val[3]'";
-	if($PlantReac=~/''/){
-	    $Nan++;
-	    $PlantReac="'NaN$Nan'";
-	}
-	#$dbh->do("insert into FichierEnsembl values($PlantReac, $Uniprot, $Transcript, $GeneStable)");
-    }
+	push(@doublon,$Uniprot);
+    }	
 }
+
+
+
+
 print "Termine \n";
 close(IN);
+$dbh->disconnect();
